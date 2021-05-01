b3054bfa378520a9d58e49e81f1a26b93bdc7529
akhand patel <akhand3108patel@gmail.com>
2021-05-01 23:38:53 +0530

    Styling FIx

--- src/components/sidebaritem/styles.js
+++ src/components/sidebaritem/styles.js
@@ -1,18 +1,21 @@
-const styles = theme => ({
+const styles = (theme) => ({
   listItem: {
-    cursor: 'pointer'
+    cursor: "pointer",
+    color: "#3c096c",
   },
   textSection: {
-    maxWidth: '85%'
-  },  
+    maxWidth: "85%",
+  },
   deleteIcon: {
-    position: 'absolute',
-    right: '5px',
-    top: 'calc(50% - 15px)',
-    '&:hover': {
-      color: 'red'
-    }
-  }
-});
+    position: "absolute",
+    right: "5px",
+    top: "calc(50% - 15px)",
+    transition: "all 0.2s ease-in-out",
+    "&:hover": {
+      color: "#10002b",
+      transform: "scale(1.3)",
+    },
+  },
+})
 
-export default styles;
+export default styles
