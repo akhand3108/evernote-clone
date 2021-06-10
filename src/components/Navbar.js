5e1d57007616d113839e64d88ee1fa8a68359309
Akhand Patel <akhand3108patel@gmail.com>
2021-06-10 12:25:29 +0000

    Feat debounced save, Navbar Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
@@ -13,7 +13,8 @@ import {
   ListItemIcon,
   ListItemText,
 } from "@material-ui/core"
-import { AccountCircleOutlined, MenuButton } from "@material-ui/icons"
+import { AccountCircleOutlined  } from "@material-ui/icons"
+import MenuIcon from "@material-ui/icons/Menu"
 
 import SidebarItem from "../SidebarItem"
 
@@ -26,15 +27,11 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-const Navbar = ({ title, updateTitle, notes }) => {
+const Navbar = ({ title, notes,id }) => {
   const classes = useStyles()
-  const [localTitle, setLocalTitle] = useState(title)
   const [isDrawerOpened, setIsDrawerOpened] = useState(false)
 
-  const inputHandler = (e) => {
-    setLocalTitle(e.target.value)
-    updateTitle(e.target.value)
-  }
+  
 
   const toggleDrawerStatus = () => {
     setIsDrawerOpened(true)
@@ -53,22 +50,10 @@ const Navbar = ({ title, updateTitle, notes }) => {
             color="inherit"
             aria-label="menu"
           >
-            <MenuButton />
+            <MenuIcon/>
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            <input
-              style={{
-                background: "transparent",
-                color: "#fff",
-                fontSize: "1.5rem",
-                outline: "none",
-                borderStyle: "none",
-                borderBottom: "1px solid #bbbbbb",
-              }}
-              type="text"
-              value={localTitle}
-              onChange={inputHandler}
-            />
+            {title}
           </Typography>
         </Toolbar>
       </AppBar>
@@ -83,11 +68,10 @@ const Navbar = ({ title, updateTitle, notes }) => {
             </ListItem>
           </List>
         </Link>
-        {console.log(notes)}
         {notes &&
           notes.map((note) => {
             return (
-              <Link to={"/" + note.id}>
+              <Link key={note.id} to={"/" + note.id}>
                 <SidebarItem note={note} />
               </Link>
             )
--- src/components/NotePage.js
+++ src/components/NotePage.js
@@ -1,40 +1,46 @@
-import { Button, ButtonGroup, Container, Grid } from "@material-ui/core"
-import React, { useEffect, useState } from "react"
+import { Button, ButtonGroup, Container, Grid ,Input} from "@material-ui/core"
+import React, { useCallback, useEffect, useRef, useState } from "react"
 import MDEditor from "@uiw/react-md-editor"
 import Navbar from "./Navbar"
 import { useHistory, useParams } from "react-router-dom"
 import { db, getTimeStamp } from "../firebase/config"
 
+import DeleteIcon from '@material-ui/icons/Delete';
 import useMediaQuery from "@material-ui/core/useMediaQuery"
 import NotePageMobile from "./NotePageMobile"
+import debounce from "../utils/helpers"
 
 function NotePage({ notes }) {
   const [value, setValue] = useState("")
+  const [title,setTitle] = useState("")
   const [note, setNote] = useState(null)
   const history = useHistory()
   const isMobile = useMediaQuery("(max-width:980px)")
   const { id } = useParams()
+  const delayedUpdate = useCallback(debounce((body)=>updateDocument(body),1500),[id])
+  const delayedTitleUpdate = useCallback(debounce((title)=>updateTitle(title),1500),[id])
 
   useEffect(() => {
-    db.collection("notes")
+   db.collection("notes")
       .doc(id)
       .get()
-      .then((snap) => {
-        console.log(snap)
+      .then((snap)=>{
         setNote(snap.data())
         setValue(snap.data().body)
+        setTitle(snap.data().title)
       })
   }, [id])
 
-  function updateDocument() {
+  
+
+  function updateDocument(body) {
     db.collection("notes")
       .doc(id)
       .update({
-        ...note,
-        body: value,
+        body: body,
         updatedAt: getTimeStamp(),
       })
-      .then(console.log("updated"))
+      .then((snap)=>console.log("updated",snap))
   }
 
   function deleteDocument() {
@@ -45,44 +51,38 @@ function NotePage({ notes }) {
     db.collection("notes")
       .doc(id)
       .update({
-        ...note,
         title,
-        body: value,
         updatedAt: getTimeStamp(),
       })
       .then(console.log("updated"))
   }
 
+  function titleFieldHandler(e){
+    setTitle(e.target.value)
+    delayedTitleUpdate(e.target.value)
+  }
+
   function editorChangeHandler(e) {
     setValue(e.target.value)
+    delayedUpdate(e.target.value)
   }
 
   return (
     <div>
-      <Navbar notes={notes} title={note?.title} updateTitle={updateTitle} />
+      <Navbar id={id} notes={notes} title={title}  />
 
       <Container>
-        <ButtonGroup
-          color={"secondary"}
-          aria-label="outlined secondary button group"
-        >
-          <Button
-            onClick={deleteDocument}
-            variant="contained"
-            color="default"
-            size="small"
-          >
-            Delete
-          </Button>
-          <Button
-            onClick={updateDocument}
-            variant="contained"
-            color="default"
-            size="small"
-          >
-            Save
-          </Button>
-        </ButtonGroup>
+        
+      <Grid container justify = "center">
+      <Input placeholder="Untitled" value={title} onChange={titleFieldHandler} />    
+      
+      <Button m={2} startIcon={<DeleteIcon/>} onClick={deleteDocument} variant="contained" color="secondary">
+        Delete
+      </Button>
+</Grid>
+     
+     
+         
 
         {isMobile ? (
           <NotePageMobile
